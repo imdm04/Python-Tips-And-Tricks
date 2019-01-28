f2b6e28b0591550f99decc822206dcab63233525
imdm04 <darshanmakadia506@gmail.com>
2019-01-28 19:47:33 +0530

    windows notification sample code

--- windows_notification.py
+++ windows_notification.py
@@ -1,8 +1,10 @@
-# from win10toast import ToastNotifier
-# toaster = ToastNotifier()
-# toaster.show_toast("Testing","Python is awesome!!!")
-
+# first you need to install win10toast library then yoiu can run this programm
 from win10toast import ToastNotifier
-import time
+
 toaster = ToastNotifier()
-toaster.show_toast("","Python is 10 seconds awsm!",icon_path="icon.ico",duration=5)
+#there are 4 parameters
+#   1) title
+#   2)  description of your notofication
+#   3) icon if you want to change or simply pass null
+#   4)  dutration in seconds
+toaster.show_toast("Title","Description",icon_path="icon.ico",duration=5)
